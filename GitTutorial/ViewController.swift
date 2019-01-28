8cc567e11cddc08ec8a8b688c7e04588ea4c909e
Beau <beau@murraymacrecords.com>
2019-01-28 09:58:24 -0600

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Beau Herron on 1/28/19.
+//  Copyright © 2019 Beau Herron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
